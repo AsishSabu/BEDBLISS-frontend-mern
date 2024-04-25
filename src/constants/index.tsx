d002b6abaf3829691977eef7b3e1925291721ca2
Asish Sabu <ashiasish1858@gmail.com>
2024-04-25 19:06:48 +0530

    owner login register verifyotp fixed

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,5 +1,5 @@
 export const BASE_URL="http://localhost:5000/api/"
 export const USER_API=BASE_URL+"user";
-export const OWNER_API=BASE_URL+"owner";
+export const OWNER_API=BASE_URL+"owners";
 export const ADMIN_API=BASE_URL+"admin";
 export const NUMBER_REGEX=/[a-z]/gi
